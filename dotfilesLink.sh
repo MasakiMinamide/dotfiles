5f991840048096b4e299ee3cceea6d2271a7f5c2
masakiminamide <masakiminamide@gmail.com>
2020-02-18 23:41:17 +0900

    i3 status config

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/i3/config ~/.config/i3
+ln -sf ~/dotfiles/polybar/config ~/.config/polybar
+ln -sf ~/dotfiles/i3status/confi ~/.config/i3status
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/plugin/Vundle.vim
