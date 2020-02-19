f88be2f7359309bc3e30cbf9122082ca9bfbcdbc
masakiminamide <masakiminamide@gmail.com>
2020-02-20 02:35:30 +0900

    add i3status & betterlockscreen and switched capslock with ctrl

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,8 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/i3/config ~/.config/i3
 ln -sf ~/dotfiles/polybar/config ~/.config/polybar
-ln -sf ~/dotfiles/i3status/confi ~/.config/i3status
+ln -sf ~/dotfiles/i3status/config ~/.config/i3status
+ln -sf ~/dotfiles/betterlockscreenrc ~/.config/
+ln -sf ~/dotfiles/i3/compton.conf ~/.config/
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/plugin/Vundle.vim
